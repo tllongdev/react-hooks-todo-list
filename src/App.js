c535550fca1c96697851dd9460168390cd38d6f6
Timothy Lee Long <tllong@Timothys-MacBook-Pro.local>
2020-04-15 15:19:31 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useContext, useEffect, useRef } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function appReducer(state, action) {
+  switch (action.type) {
+    case 'reset': {
+      return action.payload
+    }
+    case 'add': {
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          text: '',
+          completed: false,
+        },
+      ]
+    }
+    case 'delete': {
+      return state.filter(item => item.id !== action.payload)
+    }
+    case 'completed': {
+      return state.map(item =>
+        item.id === action.payload
+          ? { ...item, completed: !item.completed }
+          : item
+      )
+    }
+    case 'update': {
+      return state.map(item =>
+        item.id === action.payload ? { ...item, text: action.text } : item
+      )
+    }
+    default:
+      return state
+  }
 }
 
-export default App;
+const Context = React.createContext()
+
+function useEffectOnce(cb) {
+  const didRun = useRef(false)
+
+  useEffect(() => {
+    if (!didRun.current) {
+      cb()
+      didRun.current = true
+    }
+  })
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(appReducer, [])
+
+  useEffectOnce(() => {
+    const raw = localStorage.getItem('data')
+    dispatch({ type: 'reset', payload: JSON.parse(raw) })
+  })
+
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(state))
+  }, [state])
+
+  return (
+    <Context.Provider value={dispatch}>
+      <div className={'App'}>
+        <h1>Todos App</h1>
+        <button onClick={() => dispatch({ type: 'add' })}>New Todo</button>
+        <br />
+        <br />
+        <TodosList items={state} />
+      </div>
+    </Context.Provider>
+  )
+}
+
+function TodosList({ items }) {
+  return items.map(item => <TodoItem key={item.id} {...item} />)
+}
+
+function TodoItem({ id, completed, text }) {
+  const dispatch = useContext(Context)
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+      }}
+    >
+      <input
+        type='checkbox'
+        checked={completed}
+        onChange={() => dispatch({ type: 'completed', payload: id })}
+      />
+      <input
+        type='text'
+        // defaultValue={text}
+        value={text}
+        onChange={e =>
+          dispatch({ type: 'update', payload: id, text: e.target.value })
+        }
+      />
+      <button onClick={() => dispatch({ type: 'delete', payload: id })}>
+        Delete
+      </button>
+      {id}
+    </div>
+  )
+}
